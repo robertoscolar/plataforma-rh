f21cc114cff34572155a3734c366eb94079fef63
Roberto Scolar <robertobs1@hotmail.com>
2024-11-07 00:21:16 -0300

    Incluindo arquivos .php

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <title>Construtech Recrutamento</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/reset.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <header class="cabecalho">
+        <?php
+            include 'components/header.php';
+        ?>
+    </header>
+
+    <section class="conteudo_principal">
+        <div class="container_conteudo_principal">
+            <h2 class="subtitulo">CONSTRUTECH</h2>
+            <h1 class="titulo">SELECIONANDO OS SEUS COLABORADORES</h1>
+            <button class="botao_1">
+                <a href="#">SAIBA MAIS</a>
+            </button>
+        </div>
+    </section>
+
+
+    <footer class="rodape">
+        <?php
+            include 'components/footer.php';
+        ?>
+    </footer>
+
+</body>
+
+</html>
