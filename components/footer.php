77dc588e6fe362198ac23efa775ff9cda9529727
Roberto Scolar <robertobs1@hotmail.com>
2024-11-07 00:36:43 -0300

    Correção no footer.php

---
+++ components/footer.php
@@ -0,0 +1,33 @@
+<div class="container_rodape">
+    <div class="parceiros">
+        <h3 class="titulo_rodape titulo_parceiros">Nossos Parceiros:</h3>]
+        <div class="imagens_parceiros">
+            <img src="images/images/logo/tenda.png" alt="Tenda" class="imagem_parceiro" />
+            <img src="images/images/logo/even.png" alt="even" class="imagem_parceiro" />
+            <img src="images/images/logo/j&l.png" alt="J&L Construtora" class="imagem_parceiro" />
+        </div>
+    </div>
+
+    <div class="contatenos">
+        <h3 class="titulo_rodape titulo_contatenos">Contate-nos:</h3>
+        <p>Endereço: Av. Paulista 1337 - conj 29 - Bela Vista, São Paulo - SP, 01311200</p>
+        <p>Telefone: (11) 2987-0000</p>
+        <p class="contato_email">Email: faleconosco@construtech.com.br</p>
+    </div>
+
+    <div class="siganos">
+        <h3 class="titulo_rodape titulo_siganos">Siga-nos:</h3>
+        <div class="rede_social">
+            <img src="images/images/social/images.png" alt="Facebook" class="rede_social_imagem">
+            <p><a href="#">CONSTRUTECH_F</a></p>
+        </div>
+        <div class="rede_social">
+            <img src="images/images/social/x.jpg" alt="X" class="rede_social_imagem">
+            <p><a href="#">CONSTRUTECH_X</a></p>
+        </div>
+        <div class="rede_social">
+            <img src="images/images/social/Instagram.png" alt="Instagram" class="rede_social_imagem">
+            <p><a href="#">CONSTRUTECH_INSTA</a></p>
+        </div>
+    </div>
+</div>
