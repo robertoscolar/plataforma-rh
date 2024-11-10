1e716a86b416c28eaee70b1d4aa8127f7b5836be
Roberto Scolar <robertobs1@hotmail.com>
2024-11-10 15:27:44 -0300

    Correção de vulnerabilidade

--- php/connection.php
+++ php/connection.php
@@ -1,6 +1,9 @@
 <?php
 header("Content-Type: text/html; charset=utf-8");
 
+error_reporting(0);
+ini_set('display_errors', 0);
+
 $host = "localhost";
 $user = "root";
 $pass = "";
@@ -8,6 +11,7 @@ $db = "construtech";
 
 $conn = mysqli_connect($host, $user, $pass, $db);
 
-if ($conn->connect_error) {
-    die("Falha na conexão com o banco de dados: " . $conn->connect_error);
+if (!$conn) {
+    echo "Falha na conexão com o banco de dados. Por favor, tente novamente mais tarde.";
+    exit();
 }
