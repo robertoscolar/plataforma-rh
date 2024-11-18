a58ae95dd54180407ab3a341db589a6b97625bec
Rafaeel662Dev <rafaeltecpix@gmail.com>
2024-11-17 22:56:56 -0300

    Atualização
    QUEM SOMOS

--- quemsomos.php
+++ quemsomos.php
@@ -8,7 +8,6 @@
     <link rel="stylesheet" href="css/main.css">
     <link rel="stylesheet" href="css/quemsomos.css">
     <link rel="stylesheet" href="css/reset.css">
-
     <script src="js/validaCampos.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
@@ -17,12 +16,10 @@
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
         crossorigin="anonymous"></script>
 </head>
-
 <body>
     <header class="cabecalho">
         <?php include 'components/header.php'; ?>
     </header>
-
     <section class="conteudo_principal">
         <div class="introducao">
             <img class="introducao_imagem"></img>
