67c33721c2b0430384eb9216e0bb7bdda3e5e613
RAFAEL CARVALHO DE ARAUJO <rafaeltecpix@gmail.com>
2024-11-18 21:19:34 -0300

    Atualizacao

---
+++ quemsomos.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Construtech Recrutamento</title>
+
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/quemsomos.css">
+    <link rel="stylesheet" href="css/reset.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+</head>
+<body>
+    <header class="cabecalho">
+        <?php include 'components/header.php'; ?>
+    </header>
+    <section class="conteudo_principal">
+        
+        <div class="introducao">
+            <div class="introducao_imagem"></div>
+            <h1 class="dez_anos_exp">
+                <span class="cor-laranja">10 ANOS</span><br>
+                <span class="cor-preta">DE EXPERIÊNCIA!</span>
+                <p>Com uma década de história, nossa empresa se consolidou como líder em triagem e recrutamento especializado para o setor da construção civil.<br></p>
+                <p>Atuamos como parceira estratégica de diversas empresas, oferecendo soluções rápidas e eficientes na seleção de profissionais qualificados, garantindo que cada empresa contratante tenha acesso aos melhores talentos do mercado.</p>
+                <p>Nossa missão é proporcionar aos nossos clientes a tranquilidade de contar com um processo de recrutamento ágil e assertivo, sempre focado em entregar equipes altamente capacitadas para a execução de projetos com qualidade, segurança e dentro dos prazos estabelecidos.</p>
+            </h1>
+        </div>
+        <div class="oque_fazemos">
+        <h1>
+            <span class="cor-laranja">OQUE</span><BR>
+            <span class="cor-preta">FAZEMOS ?</span>
+        <p>Realizamos triagem e recrutamento de profissionais especializados para o setor da construção civil, selecionando talentos qualificados para diversas áreas, como alvenaria, elétrica, hidráulica, carpintaria e muito mais.</p>
+        </h1>
+        <div class="oque_fazemos_img"></div>
+        </div>
+        </div>
+    </section>
+
+
+    <!--<footer class="rodape">
+        <?php
+            include 'components/footer.php';
+        ?>
+    </footer> -->
+
+</body>
+</html>
