733f70ad7ad8cf246ed088c26b92dab8a3f50c34
Rafaeel662Dev <rafaeltecpix@gmail.com>
2024-11-21 01:07:55 -0300

    Finalização da criação 1.0 QuemSomos

--- quemsomos.php
+++ quemsomos.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,86 +9,94 @@
     <link rel="stylesheet" href="css/main.css">
     <link rel="stylesheet" href="css/quemsomos.css">
     <link rel="stylesheet" href="css/reset.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </head>
+
 <body>
     <header class="cabecalho">
         <?php include 'components/header.php'; ?>
     </header>
-    <section class="conteudo_principal">
-        
-    <div class="introducao">
-    <div class="introducao_imagem"></div>
-            <h1 class="dez_anos_exp">
-                <span class="cor-laranja">10 ANOS</span><br>
-                <span class="cor-preta">DE EXPERIÊNCIA!</span>
-                <p class="introducao_p">Com uma década de história, nossa empresa se consolidou como líder em triagem e recrutamento especializado para o setor da construção civil.<br></p>
-                <p class="introducao_p">Atuamos como parceira estratégica de diversas empresas, oferecendo soluções rápidas e eficientes na seleção de profissionais qualificados, garantindo que cada empresa contratante tenha acesso aos melhores talentos do mercado.</p>
-                <p class="introducao_p">Nossa missão é proporcionar aos nossos clientes a tranquilidade de contar com um processo de recrutamento ágil e assertivo, sempre focado em entregar equipes altamente capacitadas para a execução de projetos com qualidade, segurança e dentro dos prazos estabelecidos.</p>
-            </h1>
-        </div>
-    <div class="oque_fazemos">
-        <h1>
-            <span class="cor-laranja">OQUE</span><BR>
-            <span class="cor-preta fazemos_label">FAZEMOS ?</span>
-        <p class="oque_p">Realizamos triagem e recrutamento de profissionais especializados para o setor da construção civil, selecionando talentos qualificados para diversas áreas, como alvenaria, elétrica, hidráulica, carpintaria e muito mais.</p>
-        </h1>
-        <div class="oque_fazemos_img"></div>
-    </div>
-    </div>
-    
-    <div class="diferentes">
-        <div class="diferentes_conteudo">
-            <h1>
-                <span class="cor-laranja">O QUE</span><br>
-                <span>NOS TORNA DIFERENTES</span>
-            </h1>
-            <p class="texto_padrao diferente_text_center">
-                Somos mais do que um elo entre profissionais e projetos. Somos parceiros estratégicos que agregam valor em cada etapa do processo de recrutamento e seleção. Por isso, nos destacamos no mercado da construção civil.
-            </p>
-            <div class="diferente_text_center">
-                <p>1. Dez anos de compromisso com a excelência na seleção de profissionais e no sucesso dos nossos clientes.</p>
-                <p>2. Processo de seleção rigoroso para garantir os melhores talentos e resultados para nossos clientes.</p>
-                <p>3. Seleção eficiente de profissionais qualificados, prontos para atender seu projeto com rapidez e precisão.</p>
+
+    <main>
+        <section class="conteudo_principal">
+            <div class="introducao">
+                <div class="introducao_imagem"></div>
+                <h1 class="dez_anos_exp">
+                    <span class="cor-laranja">10 ANOS</span><br>
+                    <span class="cor-preta">DE EXPERIÊNCIA!</span>
+                    <p class="introducao_p">Com uma década de história, nossa empresa se consolidou como líder em triagem e recrutamento especializado para o setor da construção civil.<br></p>
+                    <p class="introducao_p">Atuamos como parceira estratégica de diversas empresas, oferecendo soluções rápidas e eficientes na seleção de profissionais qualificados, garantindo que cada empresa contratante tenha acesso aos melhores talentos do mercado.</p>
+                    <p class="introducao_p">Nossa missão é proporcionar aos nossos clientes a tranquilidade de contar com um processo de recrutamento ágil e assertivo, sempre focado em entregar equipes altamente capacitadas para a execução de projetos com qualidade, segurança e dentro dos prazos estabelecidos.</p>
+                </h1>
             </div>
-        </div>
-    </div>
 
-    
-    
+            <div class="oque_fazemos">
+                <h1>
+                    <span class="cor-laranja">OQUE</span><br>
+                    <span class="cor-preta fazemos_label">FAZEMOS?</span>
+                    <p class="oque_p">Realizamos triagem e recrutamento de profissionais especializados para o setor da construção civil, selecionando talentos qualificados para diversas áreas, como alvenaria, elétrica, hidráulica, carpintaria e muito mais.</p>
+                </h1>
+                <div class="oque_fazemos_img"></div>
+            </div>
 
+            <div class="diferentes">
+                <div class="diferentes_conteudo">
+                    <h1>
+                        <span class="cor-laranja">O QUE</span><br>
+                        <span>NOS TORNA DIFERENTES</span>
+                    </h1>
+                    <p class="texto_padrao diferente_text_center">
+                        Somos mais do que um elo entre profissionais e projetos. Somos parceiros estratégicos que agregam valor em cada etapa do processo de recrutamento e seleção. Por isso, nos destacamos no mercado da construção civil.
+                    </p>
+                    <div class="diferente_text_center">
+                        <p>1. Dez anos de compromisso com a excelência na seleção de profissionais e no sucesso dos nossos clientes.</p>
+                        <p>2. Processo de seleção rigoroso para garantir os melhores talentos e resultados para nossos clientes.</p>
+                        <p>3. Seleção eficiente de profissionais qualificados, prontos para atender seu projeto com rapidez e precisão.</p>
+                    </div>
+                </div>
+            </div>
 
+            <div class="depoimentos">
+                <h1>
+                    <span class="cor-laranja">DEPOIMENTOS DE</span><br>
+                    <span>EMPRESAS</span>
+                </h1>
+                <div class="depoimentos_images">
+                    <div class="depoimentos_cliente">
+                        <div class="depoimentos_cliente01"></div>
+                        <p>"Ficamos impressionados com a qualidade e agilidade da Construtech na seleção dos nossos profissionais. Recebemos exatamente o que precisávamos e o projeto seguiu sem contratempos."</p>
+                        <p>— Fernanda Lima , Coord. Engenheira, Construtora Horizonte</p>
+                    </div>
 
-    
-    </div>
-    <!-- <div class="depoimentos">
-        <h1>
-            <span class="cor-laranja">DEPOIMENTOS DE</span><BR>
-            <span >EMPRESAS</span>
-        </h1>
-        <div class="depoimentos_conteiner_images">
-            <div class="roberto"></div>
-            <div class="fernanda"></div>
-            <div class="alice"></div>
-        </div>
-    </div> -->
-    
-    
-    
-    </section>
+                    <div class="depoimentos_cliente">
+                        <div class="depoimentos_cliente02"></div>
+                        <p>A Construtech nos forneceu os profissionais certos para o nosso projeto. Foram rápidos na seleção e os trabalhadores eram altamente qualificados. Com certeza, uma parceria que faz a diferença no nosso dia a dia.</p>
+                        <p>— Alice Fernandes , RH Engenheira, Tenda Eng.</p>
+                    </div>
 
+                    <div class="depoimentos_cliente">
+                        <div class="depoimentos_cliente03"></div>
+                        <p>"Ficamos impressionados com a qualidade e agilidade da Construtech na seleção dos nossos profissionais. Recebemos exatamente o que precisávamos e o projeto seguiu sem contratempos."</p>
+                        <p>— Roberto Lima, Gerente de Obras, Construtora Alpha</p>
+                    </div>
+                </div>
+            </div>
 
+            <div class="contato">
+                <h1>
+                    <span class="cor-laranja ">QUER SABER</span><br>
+                    <span class="fazemos_label">MAIS?</span>
+                </h1>
+                <button type="button">ENTRE EM CONTATO JÁ!</button>
+            </div>
+        </section>
+    </main>
 
-    <!--<footer class="rodape">
-        <?php
-            include 'components/footer.php';
-        ?>
-    </footer> -->
-
+    <footer class="rodape">
+        <?php include 'components/footer.php'; ?>
+    </footer>
 </body>
+
 </html>
