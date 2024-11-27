3c685e8e0451fcfac4336e88063407f30b44637a
Roberto Scolar <robertobs1@hotmail.com>
2024-11-27 01:06:03 -0300

    Correção na responsividade de menu principal

--- js/script.js
+++ js/script.js
@@ -1,8 +1,29 @@
+//Abrir menu
 function showMenuItems() {
   var x = document.getElementById("menu_itens");
-  if (x.style.display === "block") {
-    x.style.display = "none";
+  if (x.className === "menu_itens") {
+    x.className += "_responsivo";
   } else {
-    x.style.display = "block";
+    x.className = "menu_itens";
   }
 }
+
+
+//Media Query Handler
+const mediaQuery = window.matchMedia("(min-width: 1200px)");
+
+function handleMediaChange(event) {
+    const element = document.getElementById("menu_itens");
+    if (!element) {
+      return;
+    }
+
+    if (event.matches) {
+        element.classList.add("menu_itens");
+        element.classList.remove("menu_itens_responsivo");
+    }
+}
+
+mediaQuery.addEventListener("change", handleMediaChange);
+
+handleMediaChange(mediaQuery);
