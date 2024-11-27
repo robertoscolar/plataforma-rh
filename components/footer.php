c8b61cae91d826c82004d030bb926dcaa5c62645
RAFAEL CARVALHO DE ARAUJO <rafaeltecpix@gmail.com>
2024-11-26 21:16:55 -0300

    Ajustando Rodape

--- components/footer.php
+++ components/footer.php
@@ -1,33 +1,35 @@
 <div class="container_rodape">
-    <div class="parceiros">
-        <h3 class="titulo_rodape titulo_parceiros">Nossos Parceiros:</h3>]
-        <div class="imagens_parceiros">
-            <img src="images/images/logo/tenda.png" alt="Tenda" class="imagem_parceiro" />
-            <img src="images/images/logo/even.png" alt="even" class="imagem_parceiro" />
-            <img src="images/images/logo/j&l.png" alt="J&L Construtora" class="imagem_parceiro" />
+
+
+    <div class="links">
+        <h3 class="titulo_rodape ">ACESSOS RÁPIDOS:</h3>
+        <p><a href="quemsomos.php">HOME</a></p>
+        <p><a href="quemsomos.php">QUEM SOMOS</a></p>
+        <p><a href="fale_conosco.php">FALE CONOSCO</a></p>
+        <p><a href="cadastro.php">TRABALHE CONOSCO</a></p>
         </div>
     </div>
 
     <div class="contatenos">
-        <h3 class="titulo_rodape titulo_contatenos">Contate-nos:</h3>
-        <p>Endereço: Av. Paulista 1337 - conj 29 - Bela Vista, São Paulo - SP, 01311200</p>
-        <p>Telefone: (11) 2987-0000</p>
+        <h3 class="titulo_rodape">FALE DIRETO CONOSCO:</h3>
+        <p>Tel: (11) 2987-0000 / 2876-9878</p>
         <p class="contato_email">Email: faleconosco@construtech.com.br</p>
+        <p>End:Av. Paulista 1337 - Bela Vista, São Paulo - SP, 01311200</p>
     </div>
 
     <div class="siganos">
-        <h3 class="titulo_rodape titulo_siganos">Siga-nos:</h3>
+        <h3 class="titulo_rodape titulo_siganos">SIGA-NOS:</h3>
         <div class="rede_social">
             <img src="images/images/social/images.png" alt="Facebook" class="rede_social_imagem">
-            <p><a href="#">CONSTRUTECH_F</a></p>
+            <p><a href="https://www.facebook.com/construtech_recrutamento/?locale=pt_BR" target="_blank">CONSTRUTECH_OFICIAL</a></p>
         </div>
         <div class="rede_social">
             <img src="images/images/social/x.jpg" alt="X" class="rede_social_imagem">
-            <p><a href="#">CONSTRUTECH_X</a></p>
+            <p><a href="https://x.com/construtech_recrutamento" target="_blank">CONSTRUTECH_OFICIAL</a></p>
         </div>
         <div class="rede_social">
             <img src="images/images/social/Instagram.png" alt="Instagram" class="rede_social_imagem">
-            <p><a href="#">CONSTRUTECH_INSTA</a></p>
+            <p><a href="https://www.instagram.com/construtech_recrutamento/" target="_blank">CONSTRUTECH_OFICIAL</a></p>
         </div>
     </div>
 </div>
