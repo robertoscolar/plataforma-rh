ad2265d1e4f16b8c6b4586016cf9259214b4489b
RAFAEL CARVALHO DE ARAUJO <rafaeltecpix@gmail.com>
2024-11-29 20:13:29 -0300

    Commit

--- components/footer.php
+++ components/footer.php
@@ -1,16 +1,25 @@
 <div class="container_rodape">
     <div class="links">
         <h3 class="titulo_rodape">ACESSOS RÁPIDOS:</h3>
-        <p><a href="quemsomos.php">HOME</a></p>
+        <p><a href="index.php">HOME</a></p>
         <p><a href="quemsomos.php">QUEM SOMOS</a></p>
         <p><a href="fale_conosco.php">FALE CONOSCO</a></p>
         <p><a href="cadastro.php">TRABALHE CONOSCO</a></p>
     </div>
     <div class="contatenos">
+
         <h3 class="titulo_rodape">FALE DIRETO CONOSCO:</h3>
-        <p>Tel: (11) 2987-0000 / 2876-9878</p>
-        <p class="contato_email">Email: faleconosco@construtech.com.br</p>
-        <p>End: Av. Paulista 1337 - Bela Vista, São Paulo - SP, 01311200</p>
+        <a href="https://web.whatsapp.com/send/?phone=5511912491187&text&type=phone_number&app_absent=0">
+            <p>Tel: (11) 2987-0000 / 2876-9878</p>
+        </a>
+
+        <a href="mailto:faleconosco@construtech.com.br">
+        <p class="contato_email">E-mail: faleconosco@construtech.com.br</p>
+        </a>
+
+        <a href="https://www.google.com/maps?q=Av.+Paulista+1337+-+Bela+Vista,+S%C3%A3o+Paulo+-+SP,+01311200">
+            <p>End: Av. Paulista 1337 - Bela Vista, São Paulo - SP, 01311200</p>
+        </a>
     </div>
     <div class="siganos">
         <h3 class="titulo_rodape titulo_contatenos">SIGA-NOS:</h3>
