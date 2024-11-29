2e01a5b87bc100a7abbc392784876385b85d000f
RAFAEL CARVALHO DE ARAUJO <rafaeltecpix@gmail.com>
2024-11-28 21:47:57 -0300

    Finalização layout rodape

--- components/footer.php
+++ components/footer.php
@@ -1,6 +1,6 @@
 <div class="container_rodape">
     <div class="links">
-        <h3 class="titulo_rodape ">ACESSOS RÁPIDOS:</h3>
+        <h3 class="titulo_rodape">ACESSOS RÁPIDOS:</h3>
         <p><a href="quemsomos.php">HOME</a></p>
         <p><a href="quemsomos.php">QUEM SOMOS</a></p>
         <p><a href="fale_conosco.php">FALE CONOSCO</a></p>
@@ -10,21 +10,30 @@
         <h3 class="titulo_rodape">FALE DIRETO CONOSCO:</h3>
         <p>Tel: (11) 2987-0000 / 2876-9878</p>
         <p class="contato_email">Email: faleconosco@construtech.com.br</p>
-        <p>End:Av. Paulista 1337 - Bela Vista, São Paulo - SP, 01311200</p>
+        <p>End: Av. Paulista 1337 - Bela Vista, São Paulo - SP, 01311200</p>
     </div>
     <div class="siganos">
-        <h3 class="titulo_rodape titulo_siganos">SIGA-NOS:</h3>
-        <div class="rede_social">
-            <img src="images/images/social/images.png" alt="Facebook" class="rede_social_imagem">
-            <p><a href="https://www.facebook.com/construtech_recrutamento/?locale=pt_BR" target="_blank">CONSTRUTECH_OFICIAL</a></p>
+        <h3 class="titulo_rodape titulo_contatenos">SIGA-NOS:</h3>
+        <div class="siganos_imgs">
+        <div>
+            <a href="https://www.facebook.com/construtech_recrutamento/?locale=pt_BR" target="_blank">
+            <img src="images/images/social/images.png" alt="Facebook">
+            </a>
         </div>
-        <div class="rede_social">
-            <img src="images/images/social/x.jpg" alt="X" class="rede_social_imagem">
-            <p><a href="https://x.com/construtech_recrutamento" target="_blank">CONSTRUTECH_OFICIAL</a></p>
+
+        <div>
+            <a href="https://x.com/construtech_recrutamento" target="_blank">
+            <img src="images/images/social/x.jpg" alt="X">
+            </a>
         </div>
-        <div class="rede_social">
-            <img src="images/images/social/Instagram.png" alt="Instagram" class="rede_social_imagem">
-            <p><a href="https://www.instagram.com/construtech_recrutamento/" target="_blank">CONSTRUTECH_OFICIAL</a></p>
+  
+        <div>
+            <a href="https://www.instagram.com/construtech_recrutamento/" target="_blank">
+                <img src="images/images/social/Instagram.png" alt="Instagram">
+            </a>
         </div>
+
+        </div>
+
     </div>
 </div>
