0b94e7af230a2d4db356673da448dcb8ccb10a98
Rafaeel662Dev <rafaeltecpix@gmail.com>
2024-11-30 18:35:04 -0300

    Trabalhando no responsivo 404.php

--- 404.php
+++ 404.php
@@ -5,8 +5,8 @@
     <title>404 - Página Não Encontrada</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/main.css">
     <link rel="stylesheet" href="css/404.css">
+    <link rel="stylesheet" href="css/main.css">
     <link rel="stylesheet" href="css/reset.css">
     <link rel="icon" type="image/x-icon" href="./images/favicon.ico">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
@@ -41,7 +41,7 @@
             <p class="mensagem_erro">A página que você está procurando não foi encontrada. <BR>Mas não se preocupe, nós
             podemos te ajudar a voltar ao início!</p>
             <button class="botao_1">
-                <a href="index.php">SAIBA MAIS</a>
+                <a href="index.php">PÁGINA INICIAL</a>
             </button>
         </div>
     </section>
