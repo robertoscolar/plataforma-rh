96beb12a3564bf7875fd06a8f89580f6811841df
Rafaeel662Dev <rafaeltecpix@gmail.com>
2024-12-07 11:58:14 -0300

    WhatsApp Icon - Main

--- index.php
+++ index.php
@@ -31,11 +31,11 @@
             <button class="botao_1">
                 <a href="quemsomos.php">SAIBA MAIS</a>
             </button>
-    <!-- <a href="https://api.whatsapp.com/send?phone=SEU_NUMERO&text=Olá!" 
+    <a href="https://api.whatsapp.com/send?phone=SEU_NUMERO&text=Olá!" 
    class="whatsapp-float" 
    target="_blank">
    <img src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg" alt="WhatsApp" class="whatsapp-icon">
-</a> -->
+</a>
         </div>
     </section>
     <footer class="rodape">
